4bbc908f96d8068212ba5cbe181fbc01e9795c3c
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-06-15 17:48:29 -0400

    Implement cli

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from message_system.message_system import Message_System
+import typer 
+import socket
+from typing import Optional, List
+from kademlia.storage import PersistentStorage
+from kademlia.network import Server
+import time
+
+app = typer.Typer()
+running_server: Server|None = None
+
+@app.command()
+def start(bootstrap_nodes: Optional[str]):
+    host_ip = socket.gethostbyname(socket.gethostname())
+    port = 8080
+    server = Server(storage=PersistentStorage())
+    server.listen(port=port, interface=host_ip)
+    if bootstrap_nodes:
+        target_host, target_port = bootstrap_nodes.split(' ')
+        server.bootstrap([(target_host, target_port)])
+
+    time.sleep(0.3)
+    running_server = server
+    print(f'Server started at {host_ip}')
+
+
+@app.command()
+def get_by_key(key: str):
+    assert running_server is not None
+    result = running_server.get(key)
+    assert result is not None
+    print(f'value is {result.decode()}')
+
+@app.command()
+def put(key: str, value: str):
+    assert running_server is not None
+    result = running_server.set(key, value)
+    print(f'info saved')
+
+
+
 
 if __name__ == "__main__":
-    print("starting")
-    ms = Message_System()
-    ms.add_to_send("testing broadcast", -1)
-
-    while True:
-        ms.receive()
+    app()
+    print('cli started')
