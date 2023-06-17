dfb1f91871b74995cbe60ae428e11f6ed33c788f
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-06-17 16:21:26 -0400

    Reimplement server and client cli

--- client.py
+++ client.py
@@ -1,33 +1,55 @@
-from http import server
-import typer 
+
 import socket
-from typing import Optional, List
-from kademlia.storage import PersistentStorage
-from kademlia.network import Server
-import time
-import threading
-
-app = typer.Typer()
-running_server: Server|None = None
+import rpyc
+import sys
 
 
+class ClientSession: 
+    def __init__(self, ip: str|None = None, port: int = 8086) -> None:
+        self.ip = ip if ip is not None else socket.gethostbyname(socket.gethostname())
+        self.port = port 
+        self.connection: rpyc.Connection|None = None
+    
+    def connect(self):
+        self.connection = rpyc.connect(self.ip, self.port, keepalive=True)
+        assert self.connection is not None and self.connection.root is not None 
+        # print(self.connection.root.__dict__)
+    
+    def get(self, key):
+        result = self.connection.root.get(key)
+        return result
+    
+    def put(self, key, value):
+        self.connection.root.set_key(key, value)
+        print(f'value putted')
 
-@app.command()
-def get(key: str):
-    server = Server()
-    result = server.get(key)
-    assert result is not None
-    print(f'value is {result.decode()}')
 
-@app.command()
-def put(key: str, value: str):
-    server = Server()
-    result = server.set(key, value)
-    print(f'info saved')
-
-
+client_session: ClientSession|None = None
 
 
 if __name__ == "__main__":
-    app()
-    # print('cli started')
+    if len(sys.argv) < 2:
+        raise Exception("Please specify ip and port to connect client")
+    
+    ip, port = sys.argv[1], sys.argv[2]
+    client_session = ClientSession(ip, int(port))
+    print('Connecting client to server')
+    client_session.connect()
+    print('Client shell started')
+    while True:
+        command = input('Expecting command: ').split(' ')
+        if command[0] == 'exit':
+            break
+        
+        args = command[1:] if len(command) >= 1 else []
+        func = getattr(ClientSession, command[0], None)
+        if func is None or not callable(func):
+            print('not finded command with that name')
+            continue
+        print(f'calling {func} with arguments: {args}')
+        result = func(client_session, *args)
+        if result:
+            print(f'Result is: {result}')
+        else:
+            print(f'command returned None')
+
--- server.py
+++ server.py
@@ -6,21 +6,17 @@ from kademlia.network import Server
 import threading
 import time
 
-app = app = typer.Typer()
 
-@app.command()
 def start(bootstrap_nodes: Optional[str] = None):
     host_ip = socket.gethostbyname(socket.gethostname())
-    port = 8080
-    server = Server(storage=PersistentStorage())
-    threading.Thread(target=server.listen, args=(port, host_ip)).start()
+    Server.init(ip=host_ip)
     time.sleep(0.3)
     if bootstrap_nodes:
         target_host, target_port = bootstrap_nodes.split(' ')
-        server.bootstrap([(target_host, target_port)])
+        Server.bootstrap([(target_host, target_port)])
     
     time.sleep(0.3)
     print(f'Server started at {host_ip}')
 
 if __name__ == '__main__':
-    app()
+    start()
