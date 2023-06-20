3e225ff0eb3b50bddcab209adc7fd5c40b0aace9
JavierOramas <javiale2000@gmail.com>
2023-06-20 13:40:38 -0400

    tests for storage delete old files

--- kademlia/tests/test_storage.py
+++ kademlia/tests/test_storage.py
@@ -2,6 +2,8 @@ from kademlia.storage import PersistentStorage
 import os
 from kademlia.utils import digest
 from pathlib import Path
+from time import sleep
+
 
 def test_set_value():
     storage = PersistentStorage()
@@ -11,10 +13,32 @@ def test_set_value():
     print(val)
     assert val == 'a'
     os.remove(Path(os.path.join(storage.db_path, str('a'))))
+    os.remove(Path(os.path.join(storage.timestamp_path, str('a'))))
+
+    storage.stop_thread()
+
+
+def test_delete_old():
+    storage = PersistentStorage(ttl=4)
+
+    storage["a"] = "b"
+    sleep(6)
+    assert storage.get('a') is None
+
+    storage['c'] = 'b'
+    sleep(3)
+    storage.get('c')
+    sleep(3)
+    assert storage.get('c') == 'b'
+
+    storage.stop_thread()
 
 
 def test_unexpected_delete():
-    storage = PersistentStorage()
+    storage = PersistentStorage(ttl=2)
     storage['1aaa'] = 'a'
     os.remove(Path(os.path.join(storage.db_path, str('1aaa'))))
-    assert storage.get('1aaa') == None
+    os.remove(Path(os.path.join(storage.timestamp_path, str('1aaa'))))
+    assert storage.get('1aaa') is None
+
+    storage.stop_thread()
