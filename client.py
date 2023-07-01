65379ec8d516c2fd524aac8c3f85b09998a8720d
JavierOramas <javiale2000@gmail.com>
2023-06-30 21:12:17 -0400

    help on the cli

--- client.py
+++ client.py
@@ -166,10 +166,19 @@ if __name__ == "__main__":
         if command[0] == 'exit':
             break
 
+        if command[0] == 'help':
+            print("""Command - args - description\n
+put - %key %value - stores %value in the network asociated with %key
+get - %key - gets the value asociated with %key
+help - * - displays this message
+exit - * - close the client
+            """)
+            continue
         args = command[1:] if len(command) >= 1 else []
         func = getattr(ClientSession, command[0], None)
         if func is None or not callable(func):
-            print(f'command {command[0]} not found')
+            print(
+                f'command {command[0]} not found, use "help" to see supported commands')
             continue
         logger.debug(f'calling {func} with arguments: {args}')
 
