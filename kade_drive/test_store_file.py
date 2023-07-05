d2d8139ba456be7e920f773698f123b599fe3e84
JavierOramas <javiale2000@gmail.com>
2023-07-05 13:45:07 -0400

    -

--- kade_drive/test_store_file.py
+++ kade_drive/test_store_file.py
@@ -2,6 +2,8 @@ from sklearn.datasets import load_diabetes
 import pandas as pd
 import pickle
 from client import ClientSession
+from time import sleep
+
 
 def test_store_df():
     X, y = load_diabetes(return_X_y=True)
@@ -19,11 +21,14 @@ def test_store_df():
 
     client_session.put("dataset_diabetes", X_pickle)
 
-
     print("DONE, Getting")
     print()
     print()
+    sleep(5)
     value_getted = client_session.get("dataset_diabetes")
 
     print('value getted ', value_getted)
     assert X.equals(value_getted)
+
+
+test_store_df()
